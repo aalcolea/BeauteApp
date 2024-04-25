d53e8403061138a27dcad89b037262e668e1a8e3
Piano117 <arjona117@hotmail.com>
2024-04-24 18:45:55 -0600

    Pantalla principal

---
+++ lib/calendar/calendarSchedule.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AgendaSchedule extends StatefulWidget {
+  const AgendaSchedule({super.key});
+
+  @override
+  State<AgendaSchedule> createState() => _AgendaScheduleState();
+}
+
+class _AgendaScheduleState extends State<AgendaSchedule> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/views/admin/agendaMain.dart
+++ lib/views/admin/agendaMain.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../../calendar/calendarSchedule.dart';
 
 class AgendaAdmin extends StatefulWidget {
   const AgendaAdmin({super.key});
@@ -10,6 +12,157 @@ class AgendaAdmin extends StatefulWidget {
 class _AgendaAdminState extends State<AgendaAdmin> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Padding(
+          padding: const EdgeInsets.only(
+            right: 85,
+          ),
+          child: Container(
+            width: null,
+            decoration: const BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.grey)),
+            ),
+            child: const Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 10),
+                  child: Icon(
+                    Icons.person_pin,
+                    size: 40,
+                    color: Color(0xFF4F2263),
+                  ),
+                ),
+                Text(
+                  'Doctor1',
+                  style: TextStyle(color: Color(0xFF4F2263), fontSize: 32),
+                ),
+              ],
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.notifications_none_outlined,
+              size: 40,
+              color: Color(0xFF4F2263),
+            ),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.home_outlined,
+              size: 40,
+              color: Color(0xFF4F2263),
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Container(
+        padding:
+            const EdgeInsets.only(right: 15, left: 15, bottom: 130, top: 40),
+        color: Colors.white,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 40),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        splashFactory: InkRipple.splashFactory,
+                        padding: const EdgeInsets.only(top: 60, bottom: 60),
+                        elevation: 10,
+                        surfaceTintColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side: const BorderSide(
+                              color: Color(0xFF4F2263), width: 2),
+                        ),
+                        backgroundColor: Colors.white,
+                      ),
+                      child: const Center(
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.event_note,
+                              size: 60,
+                              color: Color(0xFF8AB6DD),
+                            ),
+                            Text(
+                              'Para hoy',
+                              style: TextStyle(
+                                color: Color(0xFF8AB6DD),
+                                fontSize: 26,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: MediaQuery.of(context).size.width * 0.1),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        splashFactory: InkRipple.splashFactory,
+                        padding: const EdgeInsets.only(top: 60, bottom: 60),
+                        elevation: 10,
+                        surfaceTintColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side: const BorderSide(
+                              color: Color(0xFF4F2263), width: 2),
+                        ),
+                        backgroundColor: Colors.white,
+                      ),
+                      child: const Center(
+                        child: Column(
+                          children: [
+                            Icon(
+                              Icons.note_add_outlined,
+                              size: 60,
+                              color: Color(0xFF8AB6DD),
+                            ),
+                            Text(
+                              'Crear cita',
+                              style: TextStyle(
+                                color: Color(0xFF8AB6DD),
+                                fontSize: 26,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  //color: Colors.red,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: const Color(0xFF4F2263),
+                    width: 2,
+                  ),
+                ),
+                child: AgendaSchedule(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
