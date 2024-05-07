3088fe47e40258a1567a7c6dd394208be795cc17
Piano117 <arjona117@hotmail.com>
2024-05-07 13:44:16 -0600

    Calendario appointments

--- lib/calendar/calendarSchedule.dart
+++ lib/calendar/calendarSchedule.dart
@@ -55,7 +55,8 @@ class _AgendaScheduleState extends State<AgendaSchedule> {
   int? currentMonth = 0;
 
   final CalendarController _calendarSfCController = CalendarController();
-  bool isMonthView = true;
+  bool monthView = true;
+  bool weekView = false;
 
   int? visibleYear = 0;
 
@@ -76,133 +77,149 @@ class _AgendaScheduleState extends State<AgendaSchedule> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(9),
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              color: const Color(0xFF66BEC8),
-              width: MediaQuery.of(context).size.width,
-              height: 40,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                      currentMonth != null
-                          ? '${getMonthName(currentMonth!)} $visibleYear'
-                          : '${getMonthName(initMonth)} $visibleYear',
-                      textAlign: TextAlign.center,
-                      style:
-                          const TextStyle(fontSize: 30, color: Colors.white)),
-                  Row(
-                    children: [
-                      IconButton(
-                        icon: const Icon(
-                          Icons.arrow_back_ios_rounded,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          int previousMonth = currentMonth! - 1;
-                          int previousYear = visibleYear!;
-                          if (previousMonth < 1) {
-                            previousMonth = 12;
-                            previousYear--;
-                          }
-
-                          _calendarSfCController.displayDate =
-                              DateTime(previousYear, previousMonth, 1);
-                        },
-                      ),
-                      IconButton(
-                        icon: const Icon(
-                          Icons.arrow_forward_ios_rounded,
-                          color: Colors.white,
-                        ),
-                        onPressed: () {
-                          int nextMonth = currentMonth! + 1;
-                          int nextYear = visibleYear!;
-                          if (nextMonth > 12) {
-                            nextMonth = 1;
-                            nextYear++;
-                          }
-                          _calendarSfCController.displayDate =
-                              DateTime(nextYear, nextMonth, 1);
-                        },
-                      ),
-                    ],
-                  ),
-                ],
+    return Column(
+      children: [
+        Visibility(
+          visible: monthView,
+          child: Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: List.generate(
-                7,
-                (index) {
-                  return Expanded(
-                    child: Container(
-                      width: null,
-                      height: null,
-                      decoration: BoxDecoration(
-                        color: const Color(0xFF66BEC8).withOpacity(0.5),
-                        border: Border.all(width: 1, color: Colors.black54),
-                      ),
-                      child: Center(
-                        child: Text(
-                          _weekDaysInSpanish[index],
-                          style: const TextStyle(
-                            fontSize: 15,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w700,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(9),
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      color: const Color(0xFF66BEC8),
+                      width: MediaQuery.of(context).size.width,
+                      height: 40,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                              currentMonth != null
+                                  ? '${getMonthName(currentMonth!)} $visibleYear'
+                                  : '${getMonthName(initMonth)} $visibleYear',
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                  fontSize: 30, color: Colors.white)),
+                          Row(
+                            children: [
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.arrow_back_ios_rounded,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  int previousMonth = currentMonth! - 1;
+                                  int previousYear = visibleYear!;
+                                  if (previousMonth < 1) {
+                                    previousMonth = 12;
+                                    previousYear--;
+                                  }
+
+                                  _calendarSfCController.displayDate =
+                                      DateTime(previousYear, previousMonth, 1);
+                                },
+                              ),
+                              IconButton(
+                                icon: const Icon(
+                                  Icons.arrow_forward_ios_rounded,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  int nextMonth = currentMonth! + 1;
+                                  int nextYear = visibleYear!;
+                                  if (nextMonth > 12) {
+                                    nextMonth = 1;
+                                    nextYear++;
+                                  }
+                                  _calendarSfCController.displayDate =
+                                      DateTime(nextYear, nextMonth, 1);
+                                },
+                              ),
+                            ],
                           ),
-                        ),
+                        ],
                       ),
                     ),
-                  );
-                },
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: List.generate(
+                        7,
+                        (index) {
+                          return Expanded(
+                            child: Container(
+                              width: null,
+                              height: null,
+                              decoration: BoxDecoration(
+                                color: const Color(0xFF66BEC8).withOpacity(0.5),
+                                border:
+                                    Border.all(width: 1, color: Colors.black54),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  _weekDaysInSpanish[index],
+                                  style: const TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                    Expanded(
+                      child: SfCalendar(
+                        view: CalendarView.month,
+                        headerHeight: 0,
+                        initialDisplayDate: DateTime.now(),
+                        viewHeaderHeight: 0,
+                        firstDayOfWeek: 1,
+                        controller: _calendarSfCController,
+                        todayHighlightColor: const Color(0xFFDEA6CB),
+                        cellBorderColor: Colors.black54,
+                        dataSource: MeetingDataSource(getAppointments()),
+
+                        ///
+                        onViewChanged: (ViewChangedDetails details) {
+                          WidgetsBinding.instance.addPostFrameCallback((_) {
+                            int? visibleMonthController =
+                                _calendarSfCController.displayDate?.month;
+                            currentMonth = visibleMonthController;
+                            int? visibleYearController =
+                                _calendarSfCController.displayDate?.year;
+                            visibleYear = visibleYearController;
+                            setState(() {});
+                          });
+                        },
+
+                        ///
+                        onTap: (CalendarTapDetails details) {
+                          if (details.targetElement ==
+                              CalendarElement.calendarCell) {
+                            setState(() {
+                              weekView = true;
+                              monthView = false;
+                            });
+
+                            print('true');
+                          }
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-            Expanded(
-              child: SfCalendar(
-                view: CalendarView.month,
-                headerHeight: 0,
-                initialDisplayDate: DateTime.now(),
-                viewHeaderHeight: 0,
-                firstDayOfWeek: 1,
-                controller: _calendarSfCController,
-                todayHighlightColor: const Color(0xFFDEA6CB),
-                cellBorderColor: Colors.black54,
-                dataSource: MeetingDataSource(getAppointments()),
-
-                ///
-                onViewChanged: (ViewChangedDetails details) {
-                  WidgetsBinding.instance.addPostFrameCallback((_) {
-                    int? visibleMonthController =
-                        _calendarSfCController.displayDate?.month;
-                    currentMonth = visibleMonthController;
-                    int? visibleYearController =
-                        _calendarSfCController.displayDate?.year;
-                    visibleYear = visibleYearController;
-                    setState(() {});
-                  });
-                },
-
-                ///
-                onTap: (CalendarTapDetails details) {
-                  if (details.targetElement == CalendarElement.calendarCell) {
-                    print('true');
-                  }
-                },
-              ),
-            ),
-          ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
