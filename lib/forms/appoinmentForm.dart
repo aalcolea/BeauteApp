a8a9d1666ebbe8e50ffe86aac034f0d1bb679ceb
aalcolea <46306963+aalcolea@users.noreply.github.com>
2024-05-09 01:27:58 -0600

    tesT

--- lib/forms/appoinmentForm.dart
+++ lib/forms/appoinmentForm.dart
@@ -1,80 +1,344 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-class AppointmentForm extends StatelessWidget {
-  const AppointmentForm({super.key});
+class AppointmentForm extends StatefulWidget {
+  @override
+  _AppointmentFormState createState() => _AppointmentFormState();
+}
+
+class _AppointmentFormState extends State<AppointmentForm> {
+  final TextEditingController _dateController = TextEditingController();
+  final TextEditingController _timeController = TextEditingController();
+  int day = 0;
+  int month = 0;
+  int year = 0;
+
+  String? _selectedClient;
+
+  Future<void> _selectDate(BuildContext context) async {
+    DateTime? _picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2024),
+      lastDate: DateTime(2025),
+      locale: const Locale('es', 'ES'),
+      builder: (BuildContext context, Widget? child) {
+        return Theme(
+          data: ThemeData.light().copyWith(
+            colorScheme: const ColorScheme.light(
+              primary: Color(0xFFC31B36),
+            ),
+          ),
+          child: child!,
+        );
+      },
+    );
+
+    if (_picked != null) {
+      setState(() {
+        //_dateController.text = _picked.toString().split(" ")[0];
+        day = _picked.day;
+        month = _picked.month;
+        year = _picked.year;
+        _dateController.text = "$day/$month/$year";
+        print('Día seleccionado: $day');
+        print('Mes seleccionado: $month');
+        print('Año seleccionado: $year');
+      });
+    } else {
+      setState(() {
+        _dateController.text = 'Seleccione fecha';
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    int _12hrsformat = 0;
+    TimeOfDay? _picked = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+      initialEntryMode: TimePickerEntryMode.dial,
+      builder: (context, child) {
+        final Widget mediaQueryWrapper = MediaQuery(
+          data: MediaQuery.of(context).copyWith(
+            alwaysUse24HourFormat: false,
+          ),
+          child: child!,
+        );
+        final themedPicker = Theme(
+          data: ThemeData.light().copyWith(
+            colorScheme: const ColorScheme.light(
+              primary: Color(0xFFC31B36),
+              //mod el color de la bolita de seleccionar
+              onPrimary: Colors.white,
+              //mod el color del # en la bolita de seleccionar
+              onPrimaryContainer: Colors.white,
+              //mod el color del #  en el container seleccionado
+              tertiary: Color(0xFFC31B36),
+              //mod el color de la caja de AM o PM seleccionada
+              onTertiaryContainer: Colors.white,
+              // mod el color de la letra de AM o PM seleccionada
+              surface: Colors.white,
+              //mod el color del fondo del reloj
+              onSurface:
+              Colors.black, // Color del resto #s o letras en el widget
+            ),
+          ),
+          child: mediaQueryWrapper,
+        );
+
+        if (Localizations.localeOf(context).languageCode == 'es') {
+          return Localizations.override(
+            context: context,
+            locale: const Locale('es', 'US'),
+            child: themedPicker,
+          );
+        }
+
+        return themedPicker;
+      },
+    );
+
+    if (_picked != null) {
+      String period = _picked.period.toString().split('.').last;
+      if (_picked.hour > 12) {
+        _12hrsformat = _picked.hour - 12;
+      } else if (_picked.hour <= 12) {
+        _12hrsformat = _picked.hour;
+      }
+      setState(() {
+        _timeController.text = '$_12hrsformat:${_picked.minute} $period';
+      });
+    } else {
+      setState(() {
+        _timeController.text = 'Seleccione Hora';
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          const Text(
-            'Nueva cita',
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return Form(
+      child: SingleChildScrollView(
+        padding: EdgeInsets.zero,
+        child: Container(
+          color: Colors.transparent,
+          child: Stack(
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  ClipRRect(
+                    borderRadius:
+                    const BorderRadius.vertical(top: Radius.circular(16.0)),
+                    child: Container(
+                      margin: const EdgeInsets.only(bottom: 20),
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width * 1,
+                      height: 45,
+                      color: const Color(0xFF4F2263),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              padding: const EdgeInsets.only(left: 20),
+                              alignment: Alignment.center,
+                              child: const Text(
+                                'Nueva cita',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            style: IconButton.styleFrom(
+                                padding: EdgeInsets.zero,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(5),
+                                  side: const BorderSide(
+                                      width: 0.1, color: Colors.black),
+                                ),
+                                backgroundColor:
+                                Colors.black.withOpacity(0.15)),
+                            icon: const Icon(
+                              Icons.close,
+                              color: Colors.white,
+                              size: 25,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4F2263),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Text(
+                      'Cliente',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 10),
+                    child: DropdownButtonFormField(
+                      value: _selectedClient,
+                      decoration: const InputDecoration(
+                        labelText: 'Cliente',
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                      items: <String>['Cliente 1', 'Cliente 2', 'Cliente 3']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          _selectedClient = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4F2263),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Text(
+                      'Fecha',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 10),
+                    child: TextFormField(
+                      controller: _dateController,
+                      decoration: const InputDecoration(
+                        labelText: 'DD/M/AAAA',
+                        border: OutlineInputBorder(),
+                        suffixIcon: Icon(Icons.calendar_today),
+                      ),
+                      readOnly: true,
+                      onTap: () {
+                        _selectDate(context);
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4F2263),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Text(
+                      'Hora',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 10),
+                    child: TextFormField(
+                      controller: _timeController,
+                      decoration: const InputDecoration(
+                        labelText: 'HH:MM',
+                        border: OutlineInputBorder(),
+                        suffixIcon: Icon(Icons.access_time),
+                      ),
+                      readOnly: true,
+                      onTap: () {
+                        _selectTime(context);
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF4F2263),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: const Text(
+                      'Tratamiento',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 15, horizontal: 10),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Describa el tratamiento...',
+                      ),
+                      maxLines: 3,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 15),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        splashFactory: InkRipple.splashFactory,
+                        padding: EdgeInsets.zero,
+                        //elevation: 10,
+                        //surfaceTintColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          side: const BorderSide(
+                              color: Color(0xFF4F2263), width: 2),
+                        ),
+                        fixedSize: Size(
+                          MediaQuery.of(context).size.width * 0.45,
+                          MediaQuery.of(context).size.height * 0.06,
+                        ),
+                        backgroundColor: const Color(0xFFEFE6F7),
+                        //backgroundColor: const Color(0xFFC5B6CD),
+                      ),
+                      child: const Text(
+                        'Crear cita',
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
-          const SizedBox(height: 20),
-          DropdownButtonFormField(
-            decoration: const InputDecoration(
-              labelText: 'Cliente',
-              border: OutlineInputBorder(),
-              filled: true,
-              fillColor: Colors.white,
-            ),
-            items: <String>['Cliente 1', 'Cliente 2', 'Cliente 3']
-                .map<DropdownMenuItem<String>>((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            onChanged: (String? newValue) {},
-          ),
-          const SizedBox(height: 20),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Fecha',
-              hintText: 'DD/MM/AAAA',
-              border: OutlineInputBorder(),
-              suffixIcon: Icon(Icons.calendar_today),
-            ),
-            onTap: () {
-              // Aquí puedes integrar un picker de fecha
-            },
-          ),
-          const SizedBox(height: 20),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Hora',
-              hintText: 'HH:MM',
-              border: OutlineInputBorder(),
-              suffixIcon: Icon(Icons.access_time),
-            ),
-            onTap: () {
-              // Aquí puedes integrar un picker de tiempo
-            },
-          ),
-          const SizedBox(height: 20),
-          TextFormField(
-            decoration: const InputDecoration(
-              labelText: 'Tratamiento',
-              border: OutlineInputBorder(),
-              hintText: 'Describa el tratamiento...',
-            ),
-            maxLines: 3,
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-
-            },
-            style: ElevatedButton.styleFrom(
-              minimumSize: const Size(double.infinity, 50), // ancho doble
-            ),
-            child: const Text('Crear cita'),
-          ),
-        ],
+        ),
       ),
     );
   }
