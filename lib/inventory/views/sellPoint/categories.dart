813b3d522b731188e44a785ee2e76b4deb0679e9
Mac Mini <macmini@Mac-mini-de-Antonio.local>
2024-09-25 04:46:13 -0600

    search bar added to inventory screen

--- lib/inventory/views/sellPoint/categories.dart
+++ lib/inventory/views/sellPoint/categories.dart
@@ -1,12 +1,63 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 class Categories extends StatefulWidget {
-  const Categories({super.key});
+
+  final void Function(
+      bool,
+  ) onHideBtnsBottom;
+
+  const Categories({super.key, required this.onHideBtnsBottom});
 
   @override
   State<Categories> createState() => _CategoriesState();
 }
 
 class _CategoriesState extends State<Categories> {
+
+  late StreamSubscription<bool> keyboardVisibilitySubscription;
+  late KeyboardVisibilityController keyboardVisibilityController;
+  bool visibleKeyboard = false;
+  final TextEditingController searchController = TextEditingController();
+  final FocusNode focusNode = FocusNode();
+
+  void checkKeyboardVisibility() {
+    keyboardVisibilitySubscription =
+        keyboardVisibilityController.onChange.listen((visible) {
+          setState(() {
+            visibleKeyboard = visible;
+            widget.onHideBtnsBottom(visibleKeyboard);
+          });
+        });
+  }
+
+  void hideKeyBoard() {
+    if (visibleKeyboard) {
+      FocusScope.of(context).unfocus();
+    }
+  }
+
+  void _onHideBtnsBottom(bool hideBtnsBottom) {
+    setState(() {
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    keyboardVisibilityController = KeyboardVisibilityController();
+    checkKeyboardVisibility();
+  }
+
+  @override
+  void dispose() {
+    keyboardVisibilitySubscription.cancel();
+    searchController.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,10 +73,18 @@ class _CategoriesState extends State<Categories> {
                   child: SizedBox(
                     height: 37,
                     child: TextFormField(
+                      controller: searchController,
+                      focusNode: focusNode,
                       decoration: InputDecoration(
                         contentPadding: EdgeInsets.zero,
                         hintText: 'Buscar producto...',
                         prefixIcon: const Icon(Icons.search),
+                        suffixIcon: InkWell(
+                          onTap: () {
+                            print('QR code');
+                          },
+                            child: Icon(CupertinoIcons.barcode_viewfinder, color: Color(0xFF4F2263))
+                        ),
                         disabledBorder: OutlineInputBorder(
                           borderSide: BorderSide(color: const Color(0xFF4F2263).withOpacity(0.3), width: 2.0),
                           borderRadius: BorderRadius.circular(10.0),
