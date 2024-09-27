11bc5cd8031cde16797a4d1ef6e3d172f24d0878
Mac Mini <macmini@Mac-mini-de-Antonio.local>
2024-09-27 03:39:45 -0600

    Categories view created

--- lib/inventory/views/sellPoint/categories.dart
+++ lib/inventory/views/sellPoint/categories.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+import 'package:intl/intl.dart';
 class Categories extends StatefulWidget {
 
   final void Function(
@@ -22,6 +23,7 @@ class _CategoriesState extends State<Categories> {
   bool visibleKeyboard = false;
   final TextEditingController searchController = TextEditingController();
   final FocusNode focusNode = FocusNode();
+  String? _selectedCategory;
 
   void checkKeyboardVisibility() {
     keyboardVisibilitySubscription =
@@ -40,8 +42,7 @@ class _CategoriesState extends State<Categories> {
   }
 
   void _onHideBtnsBottom(bool hideBtnsBottom) {
-    setState(() {
-    });
+    setState(() {});
   }
 
   @override
@@ -59,8 +60,55 @@ class _CategoriesState extends State<Categories> {
     super.dispose();
   }
 
+  List<Map<String, dynamic>> items = [
+    {
+      'category': 'Bloqueadores',
+      'image': 'assets/imgLog/categoriesImgs/bloqueador.png'
+    },
+    {'category': 'Botox', 'image': 'assets/imgLog/categoriesImgs/botox.png'},
+    {
+      'category': 'Cremas antiarrugas',
+      'image': 'assets/imgLog/categoriesImgs/cremaAnt.png'
+    },
+    {
+      'category': 'Cremas hidratantes',
+      'image': 'assets/imgLog/categoriesImgs/cremaHidr.png'
+    },
+    {
+      'category': 'Jeringas',
+      'image': 'assets/imgLog/categoriesImgs/jeringas.png'
+    },
+    {'category': 'Jirafas', 'image': 'assets/imgLog/categoriesImgs/jirafa.png'},
+    {
+      'category': 'Sandwiches',
+      'image': 'assets/imgLog/categoriesImgs/sandwich.png'
+    },
+    {'category': 'Balones', 'image': 'assets/imgLog/categoriesImgs/balon.png'},
+    {
+      'category': 'Jeringas',
+      'image': 'assets/imgLog/categoriesImgs/jeringas.png'
+    },
+    {'category': 'Jirafas', 'image': 'assets/imgLog/categoriesImgs/jirafa.png'},
+    {
+      'category': 'Sandwiches',
+      'image': 'assets/imgLog/categoriesImgs/sandwich.png'
+    },
+    {'category': 'Balones', 'image': 'assets/imgLog/categoriesImgs/balon.png'},
+    {
+      'category': 'Bloqueadores',
+      'image': 'assets/imgLog/categoriesImgs/bloqueador.png'
+    },
+    {'category': 'Botox', 'image': 'assets/imgLog/categoriesImgs/botox.png'},
+    {
+      'category': 'Cremas antiarrugas',
+      'image': 'assets/imgLog/categoriesImgs/cremaAnt.png'
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
+    int itemsPerPage = 6;
+    int pageCount = (items.length / itemsPerPage).ceil();
     return Container(
       color: Colors.white,
       child: Column(
@@ -69,7 +117,10 @@ class _CategoriesState extends State<Categories> {
             children: [
               Expanded(
                 child: Padding(
-                  padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.025, left: MediaQuery.of(context).size.width * 0.025),
+                  padding: EdgeInsets.only(
+                      right: MediaQuery.of(context).size.width * 0.025,
+                      left: MediaQuery.of(context).size.width * 0.025
+                  ),
                   child: SizedBox(
                     height: 37,
                     child: TextFormField(
@@ -78,19 +129,21 @@ class _CategoriesState extends State<Categories> {
                       decoration: InputDecoration(
                         contentPadding: EdgeInsets.zero,
                         hintText: 'Buscar producto...',
-                        prefixIcon: const Icon(Icons.search),
-                        suffixIcon: InkWell(
-                          onTap: () {
-                            print('QR code');
-                          },
-                            child: Icon(CupertinoIcons.barcode_viewfinder, color: Color(0xFF4F2263))
+                        hintStyle: TextStyle(
+                          color: const Color(0xFF4F2263).withOpacity(0.2),
                         ),
-                        disabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: const Color(0xFF4F2263).withOpacity(0.3), width: 2.0),
-                          borderRadius: BorderRadius.circular(10.0),
+                        prefixIcon: Icon(Icons.search, color: Color(0xFF4F2263)
+                            .withOpacity(0.2)),
+                        suffixIcon: InkWell(
+                            onTap: () {
+                              print('QR code');
+                            },
+                            child: const Icon(CupertinoIcons.barcode_viewfinder,
+                                color: Color(0xFF4F2263))
                         ),
                         enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: const Color(0xFF4F2263).withOpacity(0.3), width: 2.0),
+                          borderSide: BorderSide(color: const Color(0xFF4F2263)
+                              .withOpacity(0.2), width: 2.0),
                           borderRadius: BorderRadius.circular(10.0),
                         ),
                         border: OutlineInputBorder(
@@ -103,6 +156,137 @@ class _CategoriesState extends State<Categories> {
                 ),
               )
             ],
+          ),
+          _selectedCategory == null
+              ? Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(top: MediaQuery
+                      .of(context)
+                      .size
+                      .width * 0.05),
+                  child: SizedBox(
+                    height: 580,
+                    child: PageView.builder(
+                      scrollDirection: Axis.vertical,
+                      itemCount: pageCount,
+                      itemBuilder: (context, pageIndex) {
+                        int startIndex = pageIndex * itemsPerPage;
+                        int endIndex = startIndex + itemsPerPage;
+                        if (endIndex > items.length) {
+                          endIndex = items
+                              .length; // Ajustar el índice si no hay más elementos
+                        }
+                        var currentPageItems = items.sublist(startIndex, endIndex);
+                        return GridView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          padding: EdgeInsets.only(
+                            top: MediaQuery
+                                .of(context)
+                                .size
+                                .width * 0.01,
+                            bottom: MediaQuery
+                                .of(context)
+                                .size
+                                .width * 0.01,
+                            left: MediaQuery
+                                .of(context)
+                                .size
+                                .width * 0.01,
+                            right: MediaQuery
+                                .of(context)
+                                .size
+                                .width * 0.01,
+                          ),
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 2,
+                          ),
+                          itemCount: currentPageItems.length,
+                          itemBuilder: (context, index) {
+                            var item = currentPageItems[index];
+                            return InkWell(
+                              onTap: () {
+                                setState(() {
+                                  if (mounted) {
+                                    _selectedCategory = "${item['category']}";
+                                  }
+                                });
+                                print("${item['category']}");
+                              },
+                              child: Card(
+                                  color: Colors.transparent,
+                                  shadowColor: Colors.transparent,
+                                  child: Padding(
+                                    padding: EdgeInsets.only(left: MediaQuery
+                                        .of(context)
+                                        .size
+                                        .width * 0.02, right: MediaQuery
+                                        .of(context)
+                                        .size
+                                        .width * 0.02),
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        Container(
+                                          padding: const EdgeInsets.all(1),
+                                          decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius: BorderRadius.circular(10),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color: Colors.black54.withOpacity(
+                                                    0.3),
+                                                offset: Offset(4, 4),
+                                                blurRadius: 5,
+                                                spreadRadius: 0.1,
+                                              )
+                                            ],
+                                          ),
+                                          height: MediaQuery
+                                              .of(context)
+                                              .size
+                                              .width * 0.35,
+                                          width: MediaQuery
+                                              .of(context)
+                                              .size
+                                              .width * 0.5,
+                                          child: ClipRRect(
+                                            borderRadius: BorderRadius.circular(10),
+                                            child: Image.asset(
+                                              item['image'],
+                                              fit: BoxFit.contain,
+                                            ),
+                                          ),
+                                        ),
+
+                                        const SizedBox(height: 8),
+                                        // Espacio entre imagen y texto
+                                        Text(
+                                            "${item['category']}",
+                                            style: TextStyle(
+                                              color: Color(0xFF4F2263),
+                                              fontSize: MediaQuery
+                                                  .of(context)
+                                                  .size
+                                                  .width * 0.045,
+                                            )
+                                        ),
+                                      ],
+                                    ),
+                                  )
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ) : Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(16.0),
+              color: Colors.purple,
+            ),
           )
         ],
       ),
