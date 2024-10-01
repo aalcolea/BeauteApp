0ad44edba170cf5dbba9b1f2dd39632f77da2f97
aalcolea <alanalcolea@gmail.com>
2024-10-01 04:08:04 -0600

    updates" "

--- lib/services/clienteService.dart
+++ lib/services/clienteService.dart
@@ -22,7 +22,7 @@ class ClientService {
       print('Error: $e');
     }
   }
-
+//funciones clientinfo
   Future<Map<String, dynamic>> fetchAppointmentByUser(int id) async {
     final String url = 'https://beauteapp-dd0175830cc2.herokuapp.com/api/getAppoinmentsByUser/$id';
     try {
@@ -43,6 +43,33 @@ class ClientService {
       return {
         'error': 'Ha ocurrido un error: $e',
       };
+    }
+  }
+  //funcion updateUserInfo aun en desuso, solo se migro
+  Future<void> updateUserInfo({required int id, required String token, required String name, required String phone, required String email,
+  }) async {
+    final url = Uri.parse(baseUrl + 'editUserInfo/$id');
+    try {
+      final response = await http.post(
+        url,
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode({
+          'name': name,
+          'number': phone,
+          'email': email,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        print('Datos actualizados correctamente');
+      } else {
+        print('Error al actualizar los datos: ${response.body}');
+      }
+    } catch (e) {
+      print("Error al hacer la solicitud: $e");
     }
   }
 }
