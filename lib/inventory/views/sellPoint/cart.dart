923f47d5c9b9f8c9e073d367f106648766f33579
AntonioRiosCovian <macmini@Mac-mini-de-Antonio.local>
2024-10-01 06:36:40 -0600

    cart view modified

--- lib/inventory/views/sellPoint/cart.dart
+++ lib/inventory/views/sellPoint/cart.dart
@@ -1,7 +1,16 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
 
 class Cart extends StatefulWidget {
-  const Cart({super.key});
+
+  final void Function(
+      bool,
+      ) onHideBtnsBottom;
+
+  const Cart({super.key, required this.onHideBtnsBottom});
 
   @override
   State<Cart> createState() => _CartState();
@@ -10,6 +19,42 @@ class Cart extends StatefulWidget {
 class _CartState extends State<Cart> {
 
   double totalCart = 0;
+  final FocusNode focusNode = FocusNode();
+  final TextEditingController cantidadController = TextEditingController();
+  late StreamSubscription<bool> keyboardVisibilitySubscription;
+  late KeyboardVisibilityController keyboardVisibilityController;
+  bool visibleKeyboard = false;
+
+  void checkKeyboardVisibility() {
+    keyboardVisibilitySubscription =
+        keyboardVisibilityController.onChange.listen((visible) {
+          setState(() {
+            visibleKeyboard = visible;
+            widget.onHideBtnsBottom(visibleKeyboard);
+          });
+        });
+  }
+
+  /*void hideKeyBoard() {
+    if (visibleKeyboard) {
+      FocusScope.of(context).unfocus();
+    }
+  }*/
+
+  @override
+  void initState() {
+    super.initState();
+    keyboardVisibilityController = KeyboardVisibilityController();
+    checkKeyboardVisibility();
+  }
+
+  @override
+  void dispose() {
+    keyboardVisibilitySubscription.cancel();
+    cantidadController.dispose();
+    focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,35 +62,34 @@ class _CartState extends State<Cart> {
       padding: EdgeInsets.only(top: MediaQuery.of(context).size.width * 0.015),
       child: Column(
         children: [
-          Container(
-            height: MediaQuery.of(context).size.width * 0.95,
-            padding: EdgeInsets.only(top: MediaQuery.of(context).size.width * 0.015),
-            margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.width * 0.045,
-                bottom: MediaQuery.of(context).size.width * 0.02,
-                left: MediaQuery.of(context).size.width * 0.03,
-                right: MediaQuery.of(context).size.width * 0.03
-            ),
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: BorderRadius.circular(10),
-              border: Border.all(
-                color: Color(0xFF4F2263).withOpacity(0.1),
-                width: 2,
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.width * 0.015),
+              margin: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.width * 0.01,
+                  bottom: MediaQuery.of(context).size.width * 0.01,
+                  left: MediaQuery.of(context).size.width * 0.03,
+                  right: MediaQuery.of(context).size.width * 0.03
               ),
-            ),
-            child: Stack(
-              children: [
-                LayoutBuilder(builder: (context, constraints) {
-                    final widthItem1 = constraints.maxWidth * 0.45;
-                    final widthItem2 = constraints.maxWidth * 0.29;
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(
+                  color: Color(0xFF4F2263).withOpacity(0.1),
+                  width: 2,
+                ),
+              ),
+              child: Stack(
+                children: [
+                  LayoutBuilder(builder: (context, constraints) {
+                    final widthItem1 = constraints.maxWidth * 0.41;
+                    final widthItem2 = constraints.maxWidth * 0.33;
                     return Row(
                       children: [
                         Container(
                           alignment: Alignment.topLeft,
                           padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02),
                           width: widthItem1,
-                          height: MediaQuery.of(context).size.width * 0.9,
                           decoration: BoxDecoration(
                               border: Border(right: BorderSide(color: Color(0xFF4F2263).withOpacity(0.1), width: 2))
                           ),
@@ -62,7 +106,6 @@ class _CartState extends State<Cart> {
                           alignment: Alignment.topLeft,
                           padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02),
                           width: widthItem2,
-                          height: MediaQuery.of(context).size.width * 0.9,
                           decoration: BoxDecoration(
                               border: Border(right: BorderSide(color: Color(0xFF4F2263).withOpacity(0.1), width: 2))
                           ),
@@ -78,7 +121,6 @@ class _CartState extends State<Cart> {
                         Container(
                           alignment: Alignment.topLeft,
                           padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02),
-                          height: MediaQuery.of(context).size.width * 0.9,
                           child: Text(
                             'Precio',
                             style: TextStyle(
@@ -91,30 +133,165 @@ class _CartState extends State<Cart> {
                       ],
                     );
                   }
-                ),
-                /*LayoutBuilder(
-                  builder: (context, constraints) {
-                    final widthItem1 = constraints.maxWidth * 0.45;
-                    final widthItem2 = constraints.maxWidth * 0.29;
-                    return Expanded(
-                      child: ListView.builder(
-                        itemBuilder: (context, index) {
-                          return Column(
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.width * 0.02, top: MediaQuery.of(context).size.width * 0.1),
+                    child: LayoutBuilder(
+                        builder: (context, constraints) {
+                          final widthItem1 = constraints.maxWidth * 0.382;
+                          final widthItem2 = constraints.maxWidth * 0.34;
+                          return ListView.builder(
+                            padding: EdgeInsets.zero,
+                            itemBuilder: (context, index) {
+                              return Column(
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(bottom: MediaQuery.of(context).size.width * 0.06, left: MediaQuery.of(context).size.width * 0.02),
+                                    child: Row(
+                                      children: [
+                                        Container(
+                                          width: widthItem1,
+                                          child: Column(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                'Producto $index',
+                                                style: TextStyle(
+                                                  color: Color(0xFF4F2263),
+                                                  fontSize: MediaQuery.of(context).size.width * 0.05,
+                                                ),
+                                              ),
+                                              Text(
+                                                'Codigo $index',
+                                                style: TextStyle(
+                                                    color: Color(0xFF4F2263).withOpacity(0.3),
+                                                    fontSize: MediaQuery.of(context).size.width * 0.04
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        Container(
+                                          padding: EdgeInsets.zero,
+                                          width: widthItem2,
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            crossAxisAlignment: CrossAxisAlignment.center,
+                                            children: [
+                                              Container(
+                                                padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.02),
+                                                height: MediaQuery.of(context).size.height * 0.035,
+                                                width: MediaQuery.of(context).size.width * 0.08,
+                                                child: ElevatedButton(
+                                                  style: ElevatedButton.styleFrom(
+                                                    backgroundColor: const Color(0xFF4F2263).withOpacity(0.5),
+                                                    padding: EdgeInsets.symmetric(
+                                                      horizontal: MediaQuery.of(context).size.width * 0.005,
+                                                    ),
+                                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0),
+                                                    ),
+                                                  ),
+                                                  onPressed: () {
 
+                                                  },
+                                                  child: Icon(
+                                                    CupertinoIcons.minus,
+                                                    color: Colors.white,
+                                                    size: MediaQuery.of(context).size.width * 0.04,
+                                                  ),
+                                                ),
+                                              ),
+                                              Container(
+                                                alignment: Alignment.center,
+                                                width: MediaQuery.of(context).size.width * 0.07,
+                                                height: MediaQuery.of(context).size.height * 0.035,
+                                                decoration: BoxDecoration(
+                                                  borderRadius: BorderRadius.circular(5),
+                                                  border: Border.all(
+                                                      color: Color(0xFF4F2263).withOpacity(0.1),
+                                                      width: 2
+                                                  ),
+                                                ),
+                                                child: TextFormField(
+                                                  controller: cantidadController,
+                                                  focusNode: focusNode,
+                                                  textAlign: TextAlign.center,
+                                                  textAlignVertical: TextAlignVertical.center,
+                                                  //initialValue: '$index',
+                                                ),
+                                              ),
+                                              Container(
+                                                padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.02),
+                                                height: MediaQuery.of(context).size.height * 0.035,
+                                                width: MediaQuery.of(context).size.width * 0.08,
+                                                child: ElevatedButton(
+                                                  style: ElevatedButton.styleFrom(
+                                                    backgroundColor: const Color(0xFF4F2263).withOpacity(0.5),
+                                                    padding: EdgeInsets.symmetric(
+                                                      horizontal: MediaQuery.of(context).size.width * 0.005,
+                                                    ),
+                                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0),
+                                                    ),
+                                                  ),
+                                                  onPressed: () {
+
+                                                  },
+                                                  child: Icon(
+                                                    CupertinoIcons.add,
+                                                    color: Colors.white,
+                                                    size: MediaQuery.of(context).size.width * 0.045,
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: Container(
+                                            padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.02),
+                                            alignment: Alignment.topRight,
+                                            child: Column(
+                                              mainAxisAlignment: MainAxisAlignment.center,
+                                              crossAxisAlignment: CrossAxisAlignment.end,
+                                              children: [
+                                                Text(
+                                                  '\$0',
+                                                  style: TextStyle(
+                                                    color: Color(0xFF4F2263),
+                                                    fontSize: MediaQuery.of(context).size.width * 0.05,
+                                                  ),
+                                                ),
+                                                Text(
+                                                  'MXN',
+                                                  style: TextStyle(
+                                                      color: Color(0xFF4F2263).withOpacity(0.3),
+                                                      fontSize: MediaQuery.of(context).size.width * 0.04
+                                                  ),
+                                                )
+                                              ],
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  )
+                                ],
+                              );
+                            },
                           );
-                        },
-                      ),
-                    );
-                  }
-                )*/
-              ],
+                        }
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           Padding(
             padding: EdgeInsets.only(
               left: MediaQuery.of(context).size.width * 0.03,
               right: MediaQuery.of(context).size.width * 0.03,
-              top: MediaQuery.of(context).size.width * 0.01,
+              top: MediaQuery.of(context).size.width * 0.0,
               bottom: MediaQuery.of(context).size.width * 0.01
             ),
             child: Row(
@@ -126,7 +303,7 @@ class _CartState extends State<Cart> {
                   style: TextStyle(
                     color: Color(0xFF4F2263),
                     fontWeight: FontWeight.bold,
-                    fontSize: MediaQuery.of(context).size.width * 0.1,
+                    fontSize: MediaQuery.of(context).size.width * 0.08,
                   ),
                 ),
                 Column(
@@ -137,7 +314,7 @@ class _CartState extends State<Cart> {
                       style: TextStyle(
                         color: Color(0xFF4F2263),
                         fontWeight: FontWeight.bold,
-                        fontSize: MediaQuery.of(context).size.width * 0.1,
+                        fontSize: MediaQuery.of(context).size.width * 0.08,
                       ),
                     ),
                     Text(
@@ -145,7 +322,7 @@ class _CartState extends State<Cart> {
                       style: TextStyle(
                         color: Color(0xFF4F2263).withOpacity(0.3),
                         fontWeight: FontWeight.bold,
-                        fontSize: MediaQuery.of(context).size.width * 0.05,
+                        fontSize: MediaQuery.of(context).size.width * 0.04,
                       ),
                     )
                   ],
@@ -155,9 +332,9 @@ class _CartState extends State<Cart> {
           ),
           Container(
             alignment: Alignment.center,
-            height: MediaQuery.of(context).size.width * 0.17,
+            height: MediaQuery.of(context).size.width * 0.15,
             margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.width * 0.045,
+                top: MediaQuery.of(context).size.width * 0.01,
                 bottom: MediaQuery.of(context).size.width * 0.02,
                 left: MediaQuery.of(context).size.width * 0.03,
                 right: MediaQuery.of(context).size.width * 0.03
