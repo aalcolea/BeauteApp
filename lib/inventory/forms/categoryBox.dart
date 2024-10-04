90cb289b1c2462aa9ad0d8918b45e5286b902e0f
aalcolea <alanalcolea@gmail.com>
2024-10-04 05:22:21 -0600

    addigt fetch to catorgies

--- lib/inventory/forms/categoryBox.dart
+++ lib/inventory/forms/categoryBox.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class CategoryBox extends StatefulWidget {
   const CategoryBox({super.key});
@@ -9,69 +11,88 @@ class CategoryBox extends StatefulWidget {
 }
 
 class _CategoryBoxState extends State<CategoryBox> {
-
-  List<String> categories = [
-    'Crema',
-    'Botox',
-    'Loquesea',
-  ];
-
-  String ? categorySel;
+  Map<String, dynamic>? categorySel;
+  List<Map<String, dynamic>> items = [];
 
   @override
+  void initState() {
+    super.initState();
+    fetchItems();
+  }
+  ///RECORDAR MANDAR A SERVCIO
+  Future<void> fetchItems({int limit = 100, int offset = 0}) async {
+    final String baseURL = 'https://beauteapp-dd0175830cc2.herokuapp.com/api/categories';
+    final response = await http.get(Uri.parse(baseURL + '?limit=$limit&offset=$offset'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body)['data'];
+      setState(() {
+        items = data.map((item) {
+          return {
+            'id': item['id'],
+            'category': item['nombre'],
+            'image': item['foto'],
+          };
+        }).where((item) => item['category'] != null).toList();
+      });
+    } else {
+      throw Exception('Error al obtener datos de la API');
+    }
+  }
+  @override
   Widget build(BuildContext context) {
-    return DropdownButtonFormField(
-          isExpanded: true,
+    return DropdownButtonFormField<Map<String, dynamic>>(
+      isExpanded: true,
       hint: const Text('Categoria del producto'),
       value: categorySel,
-          items: categories.map((nameCategory) {
-            return DropdownMenuItem(
-                value: nameCategory,
-                child: Align(
-                  alignment: Alignment.centerLeft,//esta alineacion es para las opciones
-                  child: Text(
-                    nameCategory,
-                  ),
-                ));
-          }).toList(),
-        onChanged: (selection) {
-          setState(() {
-            categorySel = selection;
-          });
-        },
-        decoration: InputDecoration(
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: const BorderSide(
-              color: Color(0xFF4F2263),
-              width: 1.5,
+      items: items.map((categoryItem) {
+        return DropdownMenuItem<Map<String, dynamic>>(
+          value: categoryItem,
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              categoryItem['category'] ?? 'Categoria Null',
+              style: const TextStyle(fontSize: 16),
             ),
           ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: const BorderSide(
-              color: Colors.black54,
-              width: 1.5,
-            ),
+        );
+      }).toList(),
+      onChanged: (selectedCategory) {
+        setState(() {
+          categorySel = selectedCategory;
+        });
+      },
+      decoration: InputDecoration(
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            color: Color(0xFF4F2263),
+            width: 1.5,
           ),
         ),
-        style: const TextStyle(fontSize: 18, color: Color(0xFF48454C)),
-        icon: const Icon(Icons.arrow_drop_down),
-          selectedItemBuilder: (BuildContext context) {
-            return categories.map((String nameCategory) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    nameCategory,
-                    style: const TextStyle(
-                      color: Colors.black
-                    ),
-                  ),
-                ],
-              );
-            }).toList();
-          },
-      );
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: const BorderSide(
+            color: Colors.black54,
+            width: 1.5,
+          ),
+        ),
+      ),
+      style: const TextStyle(fontSize: 18, color: Color(0xFF48454C)),
+      icon: const Icon(Icons.arrow_drop_down),
+      selectedItemBuilder: (BuildContext context) {
+        return items.map((categoryItem) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                categoryItem['category'] ?? 'Categoria Null',
+                style: const TextStyle(color: Colors.black),
+              ),
+            ],
+          );
+        }).toList();
+      },
+    );
   }
 }
