b085f5d2887e75f1a926cbd63b4e93826fcf9c11
aalcolea <alanalcolea@gmail.com>
2024-10-13 19:20:52 -0600

    add

--- lib/inventory/testPrinter/testPrint.dart
+++ lib/inventory/testPrinter/testPrint.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 
@@ -17,14 +16,24 @@ class _testPrintState extends State<testPrint> {
   @override
   void initState() {
     super.initState();
+    checkConnectedDevices();
     scanForDevices();
+  }
+  void checkConnectedDevices() async {
+    List<BluetoothDevice> connectedDevices = await flutterBlue.connectedDevices;
+    if (connectedDevices.isNotEmpty) {
+      setState(() {
+        selectedDevice = connectedDevices.first;
+      });
+      discoverServices(selectedDevice!);
+    }
   }
 
   void scanForDevices() {
     flutterBlue.startScan(timeout: Duration(seconds: 5));
 
-    flutterBlue.scanResults.listen((results){
-      for(ScanResult r in results){
+    flutterBlue.scanResults.listen((results) {
+      for (ScanResult r in results) {
         if (!devicesList.contains(r.device)) {
           setState(() {
             devicesList.add(r.device);
@@ -35,12 +44,14 @@ class _testPrintState extends State<testPrint> {
 
     flutterBlue.stopScan();
   }
+
   void sendMessage(String message) async {
     if (characteristic != null) {
       List<int> bytes = utf8.encode(message + "\n");
       await characteristic!.write(bytes, withoutResponse: true);
     }
   }
+
   void discoverServices(BluetoothDevice device) async {
     List<BluetoothService> services = await device.discoverServices();
     for (BluetoothService service in services) {
@@ -88,7 +99,7 @@ class _testPrintState extends State<testPrint> {
                 children: [
                   TextField(
                     controller: messageController,
-                    decoration: InputDecoration(labelText:  "mensaje "),
+                    decoration: InputDecoration(labelText: "mensaje "),
                   ),
                   SizedBox(height: 10),
                   ElevatedButton(
